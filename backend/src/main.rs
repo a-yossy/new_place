6d2fc3b668274746befb621a9d5cbc4ad7dad40d
a-yossy <atsuya0930@yahoo.co.jp>
2025-03-23 16:56:21 +0900

    feat: backend 環境構築

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, http::GraphiQLSource};
+use async_graphql_axum::GraphQL;
+use axum::{
+    Router,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+use tokio::net::TcpListener;
+
+async fn graphiql() -> impl IntoResponse {
+    Html(GraphiQLSource::build().endpoint("/graphql").finish())
+}
+
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn test(&self) -> String {
+        "test".to_string()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    let app = Router::new().route("/graphql", get(graphiql).post_service(GraphQL::new(schema)));
+    axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
+        .await
+        .unwrap();
 }
