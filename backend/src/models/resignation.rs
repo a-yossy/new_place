8e86406280c44b97da7eb425b57cd959695d6e57
a-yossy <atsuya0930@yahoo.co.jp>
2025-03-30 01:46:23 +0900

    ...

--- backend/src/models/resignation.rs
+++ backend/src/models/resignation.rs
@@ -84,35 +84,46 @@ impl Resignation {
             return Err(anyhow!("有給がありません"));
         }
 
-        remaining_paid_leave_days -= 1;
+        if !is_holiday(holidays, &vacation_start_date) {
+            remaining_paid_leave_days -= 1;
+        }
 
         while remaining_paid_leave_days > 0 {
             let new_date = vacation_start_date
                 .checked_sub_days(Days::new(1))
                 .context("invalid date")?;
             vacation_start_date = new_date;
-            if holidays.contains_key(&new_date.to_string()) {
-                continue;
-            }
-            if new_date.weekday() == Weekday::Sat {
-                continue;
-            }
-            if new_date.weekday() == Weekday::Sun {
-                continue;
-            }
 
-            remaining_paid_leave_days -= 1;
+            if !is_holiday(holidays, &new_date) {
+                remaining_paid_leave_days -= 1;
+            }
         }
 
         Ok(vacation_start_date)
     }
 }
 
+fn is_holiday(holidays: &HashMap<String, String>, date: &NaiveDate) -> bool {
+    if holidays.contains_key(&date.to_string()) {
+        return true;
+    }
+    if date.weekday() == Weekday::Sat {
+        return true;
+    }
+    if date.weekday() == Weekday::Sun {
+        return true;
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::Result;
     use chrono::{NaiveDate, NaiveDateTime};
     use sqlx::MySqlPool;
+    use tokio::test;
 
     use crate::models::resignation::Resignation;
 
@@ -169,5 +180,73 @@ mod tests {
             NaiveDate::from_ymd_opt(2025, 1, 1).unwrap()
         );
         assert_eq!(resignations.first().unwrap().remaining_paid_leave_days, 10);
+    }
+
+    #[test]
+    async fn vacation_start_date_error() -> Result<()> {
+        let resignation = Resignation {
+            id: 1,
+            retirement_date: NaiveDate::from_ymd_opt(2025, 1, 1).unwrap(),
+            remaining_paid_leave_days: 0,
+            created_at: NaiveDateTime::parse_from_str("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")?,
+        };
+        let holidays = HashMap::new();
+
+        let result = resignation.vacation_start_date(&holidays).await;
+
+        assert!(result.is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    async fn vacation_start_date() -> Result<()> {
+        let resignation = Resignation {
+            id: 1,
+            retirement_date: NaiveDate::from_ymd_opt(2025, 1, 1).unwrap(),
+            remaining_paid_leave_days: 10,
+            created_at: NaiveDateTime::parse_from_str("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")?,
+        };
+        // 土曜日: 2024-12-28, 2024-12-21, 2024-12-14
+        // 日曜日: 2024-12-29, 2024-12-22, 2024-12-15
+        let mut holidays = HashMap::new();
+        holidays.insert("2025-01-01".to_string(), "休み".to_string());
+        holidays.insert("2024-12-31".to_string(), "休み".to_string());
+        holidays.insert("2024-12-29".to_string(), "休み".to_string());
+        holidays.insert("2024-12-28".to_string(), "休み".to_string());
+
+        let result = resignation.vacation_start_date(&holidays).await;
+
+        println!("{:?}", result.iter().clone());
+        assert!(result.is_ok());
+        assert!(result.is_ok_and(|vacation_start_date| vacation_start_date
+            == NaiveDate::from_ymd_opt(2024, 12, 17).unwrap()));
+
+        Ok(())
+    }
+
+    #[test]
+    async fn vacation_start_date2() -> Result<()> {
+        let resignation = Resignation {
+            id: 1,
+            retirement_date: NaiveDate::from_ymd_opt(2025, 1, 1).unwrap(),
+            remaining_paid_leave_days: 10,
+            created_at: NaiveDateTime::parse_from_str("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")?,
+        };
+        // 土曜日: 2024-12-28, 2024-12-21, 2024-12-14
+        // 日曜日: 2024-12-29, 2024-12-22, 2024-12-15
+        let mut holidays = HashMap::new();
+        holidays.insert("2024-12-31".to_string(), "休み".to_string());
+        holidays.insert("2024-12-29".to_string(), "休み".to_string());
+        holidays.insert("2024-12-28".to_string(), "休み".to_string());
+
+        let result = resignation.vacation_start_date(&holidays).await;
+
+        println!("{:?}", result.iter().clone());
+        assert!(result.is_ok());
+        assert!(result.is_ok_and(|vacation_start_date| vacation_start_date
+            == NaiveDate::from_ymd_opt(2024, 12, 18).unwrap()));
+
+        Ok(())
     }
 }
